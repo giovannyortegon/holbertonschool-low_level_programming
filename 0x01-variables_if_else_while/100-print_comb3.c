0c7572e447b6818d5c977e74dc583c52a46c3801
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-20 18:34:22 -0500

    Adding file 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+int b;
+for (a = 48; a <= 57; a++)
+{
+    for (b = 48; b <=57; b++)
+    {
+	if(a != b && b > a)
+	{
+	    putchar(a);
+	    putchar(b);
+	    if (a != 56 || b != 57)
+	    {
+	        putchar(',');
+		putchar(' ');
+	    }
+	}
+    }
+}
+putchar('\n');
+return 0;
+}
