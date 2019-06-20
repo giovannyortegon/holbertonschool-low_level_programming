b0c54a0cf1e587ef7104e80d63d614f8e9002120
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-20 15:30:57 -0500

    Add 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+for (a = 97; a < 103; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
