dc5dd3a3785d1ab4193b0c33743550235630f939
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-20 12:54:20 -0500

    Adding file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+for( int a = 97; a < 123; a++ )
+{
+    putchar(a);
+}
+putchar('\n');
+return (0);
+}
