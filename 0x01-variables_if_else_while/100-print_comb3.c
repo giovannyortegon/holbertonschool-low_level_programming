a1107ebf7a2f613b99fb366705dd9cf62b70f194
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-20 18:37:38 -0500

    Updating file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,20 +11,20 @@ int a;
 int b;
 for (a = 48; a <= 57; a++)
 {
-    for (b = 48; b <=57; b++)
-    {
-	if(a != b && b > a)
-	{
-	    putchar(a);
-	    putchar(b);
-	    if (a != 56 || b != 57)
-	    {
-	        putchar(',');
-		putchar(' ');
-	    }
-	}
-    }
+for (b = 48; b <= 57; b++)
+{
+if (a != b && b > a)
+{
+putchar(a);
+putchar(b);
+if (a != 56 || b != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 }
 putchar('\n');
-return 0;
+return (0);
 }
