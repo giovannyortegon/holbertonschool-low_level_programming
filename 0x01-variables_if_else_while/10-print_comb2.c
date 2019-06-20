149e69cacd9a838a15528759c9c10c6c6ab9eb79
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-20 17:02:58 -0500

    Adding file 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+int b;
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+putchar(a);
+putchar(b);
+if (a != 57 || b != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
