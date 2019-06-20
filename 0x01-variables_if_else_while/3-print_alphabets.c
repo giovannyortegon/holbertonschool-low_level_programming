da421f3c92ca8bb1f9f8de5cb6b5443a1b205f5d
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-20 14:06:38 -0500

    Adding 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 97; a < 123; a++)
+{
+putchar(a);
+}
+for (a = 65; a < 91 ; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
