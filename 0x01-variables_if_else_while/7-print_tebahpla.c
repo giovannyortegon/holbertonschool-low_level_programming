867277cf1deb748296e6caf1e048741579ccd5d1
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-20 15:13:06 -0500

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 122; a > 96; a--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
