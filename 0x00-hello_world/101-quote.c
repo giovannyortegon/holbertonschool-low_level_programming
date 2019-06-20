a32ca4b47503e184d1573d21e4676ee917892a97
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-19 22:55:15 -0500

    Updating

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, a, (61));
+write(2, "and that piece of art is useful\" -\
+Dora Korpar, 2015-10-19\n", (60));
 return (1);
 }
