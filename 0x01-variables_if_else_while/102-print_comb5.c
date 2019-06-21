e353feb8418ec6ef84dc9a4f08c078f784ac6586
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-20 21:46:00 -0500

    Adding file 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b, c, d;
+for (a = 48; a < 58; a++)
+{
+    for (b = 48; b < 58; b++)
+    {
+	for (c = 48; c < 58; c++)
+	{
+	    for (d = 48; d < 58; d++)
+	    {
+	     if ( a <= c && b < d )
+	     {
+		putchar(a);
+		putchar(b);
+		putchar(' ');
+		putchar(c);
+		putchar(d);
+		if (a !=57 || b != 56 || c != 57 || d !=57 )
+		{
+		    putchar(',');
+		    putchar(' ');
+		}
+	      }
+	    }
+	}
+    }
+}
+
+putchar('\n');
+return (0);
+}
