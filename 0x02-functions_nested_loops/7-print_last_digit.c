5178e3cd4608ee56a5408d5b9d41429b4ca35c46
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-25 12:58:00 -0500

    Adding file 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * pirnt_last_digit - main function
+ * @n: Entry variable
+ * Return: variable mod return value
+ */
+int print_last_digit(int n)
+{
+	int mod;
+	char num;
+
+	if(n < 0)
+	{
+		mod = (n % 10) *(-1);
+		num = (mod + '0');
+		_putchar(num);
+	}else
+	{
+		mod = n % 10;
+		num = (mod + '0');
+		_putchar(num);
+	} 
+	return mod;
+}
