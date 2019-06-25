45e6660622929c29844b552fb4d9f18fd1790f69
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-25 14:47:13 -0500

    Adding file 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Entry point
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a <= '2'; a++ )
+	{
+		for (b = '0'; b <= '3'; b++ )
+		{
+			for (c = '0'; c <= '5'; c++)
+			{
+				for (d ='0'; d <= '9'; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
