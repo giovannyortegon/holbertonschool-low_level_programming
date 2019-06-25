e7c8060782c2ee502b21e931e9cfadcf0e1d3835
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-25 14:49:39 -0500

    Updating file 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,13 +7,13 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = '0'; a <= '2'; a++ )
+	for (a = '0'; a <= '2'; a++)
 	{
-		for (b = '0'; b <= '3'; b++ )
+		for (b = '0'; b <= '3'; b++)
 		{
 			for (c = '0'; c <= '5'; c++)
 			{
-				for (d ='0'; d <= '9'; d++)
+				for (d = '0'; d <= '9'; d++)
 				{
 					_putchar(a);
 					_putchar(b);
