d8abc7a4d13f3d16769ce766dd2593cb8b2abd62
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-25 21:47:41 -0500

    Adding 1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	/**
+	 * @i: variables is defined
+	 */
+	int i;
+	/**
+	 * printf - show out
+	 */
+	printf("Infinite loop incoming :(\n");
+	/**
+	 * @i: is decared
+	 */
+	i = 0;
+	/**
+	* while - start the loop
+	*/
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	/**
+	 * printf - Show finished out
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
