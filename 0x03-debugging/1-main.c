99f0536250f3c91e524e609718e81e3d1e9157f1
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-25 21:59:56 -0500

    Adding file 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-	/* @i: variables is defined */
 	int i;
+
 	/* printf - show out */
 	printf("Infinite loop incoming :(\n");
-	/* @i: is decared */
+
 	i = 0;
-	/* while - start the loop */
 	while (i < 10)
 	{
 		putchar(i);
 	}
 	/* printf - Show finished out */
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
