7a50274fe6bde823c2dcd663d7dcc8603168b80a
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-27 13:35:45 -0500

    Adding 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_square - Entry point
+ * @size: size of draw
+ */
+
+void print_square(int size)
+{
+	/* @i: number of rows */
+	int i;
+	/* @j: number of column */
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= size - 1; i++)
+		{
+			for (j = 0; j < size ; j++)
+			{
+				_putchar(' ');
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
