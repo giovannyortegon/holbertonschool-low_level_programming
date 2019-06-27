6867b46d512c5e036580e558f18e6c3bc2218144
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-27 11:03:12 -0500

    Adding 4-printmost_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Entry points
+ */
+voidy print_most_numbers(void)
+{
+	/* @i: store number of ASCII*/
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == '2' || i == '4')
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
