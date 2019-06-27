4637e7179633a71b6dd093d63f675e76e3857124
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-27 18:13:01 -0500

    Updatin file 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,31 @@
 #include "holberton.h"
 
+/**
+ * more_numbers - Entry point
+ */
 void more_numbers(void)
 {
 	int i, d1, d2;
-	
+
 	i = 0;
 	while (i < 10)
 	{
 		for (d1 = '0'; d1 <= '1'; d1++)
 		{
 			for (d2 = '0'; d2 <= '9'; d2++)
-	 		{
+			{
 				if (d1 != '0')
 				{
 					_putchar(d1);
 				}
 				_putchar(d2);
-				if ( d1 == '1' && d2 == '4' )
+				if (d1 == '1' && d2 == '4')
 				{
 					break;
 				}
-		 	}
+			}
 		}
-		i++;		
+		i++;
 		_putchar('\n');
 	}
 
