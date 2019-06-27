977c4a1767739bbf6038bdee9db6558016e61862
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-27 10:18:10 -0500

    Adding 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - Entry point
+ * @a: Integer multplicand
+ * @b: Integer multipler
+ * Return: Multiplication between a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
