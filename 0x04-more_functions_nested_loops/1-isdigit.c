0e997ba81cb9970429132bb26131f700d0ce2978
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-27 10:10:44 -0500

    Adding comments at the code

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * _isdigit - Entry point
+ * @c: store varible to test
+ *
+ * Return: 1 if it a digit or 0 otherwise.
+ */
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
