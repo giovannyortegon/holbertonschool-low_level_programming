00d43e00dd36150f70c20830127dbbfa5fa2f55c
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-27 12:36:32 -0500

    Adding 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_line - Entry point
+ * @n: count a number times that need to print '_'
+ */
+void print_line(int n)
+{
+	/* @i: store repeating time */
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
