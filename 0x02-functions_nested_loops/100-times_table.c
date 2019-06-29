f9fbe9cf40423d28e08061cc1273e7a57c65734f
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-29 15:48:21 -0500

    Update file 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * times_table - Entry point
  */
@@ -7,62 +6,48 @@ void print_times_table(int n)
 {
 	int a, b, c, d1, d2, d3;
 
-	if (n >= 0 && n <= 15 )
+	if (n >= 0 && n <= 15)
 	{
-						
-		for (a = 0; a <= n; a++)
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
 		{
-			for( b = 0; b <= n; b++)
-			{
-				c = a * b;
-						
-				if ( c >= 0 && c <=9)
-				{
-					if ( b != 0 && c >= 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-	
-					d1 = c;
-					_putchar(d1 + '0');
-				}
-				else
-				{
-					if (c >= 10 && c <=99)
-					{
-						if ( c >= 10)
-						{
-							_putchar(' ');
-							_putchar(' ');
-						}
-						d1 = (c /10);
-						_putchar(d1 + '0');
-						d2 = (c % 10);
-						_putchar(d2 + '0');
-					}
-					else
-					{
-						if(c >= 100)
-						{
-							_putchar(' ');
-						}
-						d1 = (c / 100);
-						_putchar(d1 + '0');
-						d2 = ((c / 10) % 10);
-						_putchar(d2 + '0');
-						d3 = (c % 10);
-						_putchar(d3 + '0');
-					}
-				}if (b != n)
-				{
-					_putchar(',');
-				
-				}
-						
-			}	
-			_putchar('\n');
+		c = a * b;
+
+		if (c >= 0 && c <= 9)
+		{
+			if (b != 0 && c >= 0)
+				_putchar(' '), _putchar(' '), _putchar(' ');
+			_putchar(c + '0');
+		}
+		else
+		{
+		if (c >= 10 && c <= 99)
+		{
+		if (c >= 10)
+		{
+			_putchar(' '), _putchar(' ');
+		}
+		d1 = (c / 10), 	_putchar(d1 + '0');
+		d2 = (c % 10), 	_putchar(d2 + '0');
+		}
+		else
+		{
+		if (c >= 100)
+		{
+			_putchar(' ');
+		}
+		d1 = (c / 100), _putchar(d1 + '0');
+		d2 = ((c / 10) % 10), _putchar(d2 + '0');
+		d3 = (c % 10), _putchar(d3 + '0');
+		}
+		}
+		if (b != n)
+		{
+			_putchar(',');
+		}
+		}
+		_putchar('\n');
 		}
 	}
 }
