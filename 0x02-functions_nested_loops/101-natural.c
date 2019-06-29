474cc2579a0178b2a01b2bc9a6f48903a8096d0e
Skillhh <ortegon.giovanny@hotmail.com>
2019-06-29 16:07:08 -0500

    Adding 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,  mul, sum;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+		{
+			sum += a;
+		}
+	}	
+	printf("%d\n", sum);
+	return (0);
+}
