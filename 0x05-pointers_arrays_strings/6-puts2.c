8759ba7cd41ea8f41ab9c0a16023740039aee7f7
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-02 10:01:59 -0500

    Adding 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts2 - entry point
+ *
+ * @str: Entry pointer
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
