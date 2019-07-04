da2a0173889ff11251cbeeb02f888aade47d7166
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-04 17:09:46 -0500

    Updating file 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,9 +8,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int res;
-
-	res = s1[0] - s2[0];
+	int i, res;
+	
+	res = 0;
+	
+	for (i = 0; s1[i] != '\0' && res == 0; i++)
+		res = s1[i] - s2[i];
 
 	return (res);
 }
