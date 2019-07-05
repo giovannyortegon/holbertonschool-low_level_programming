2bda89bbd81197b00056f6b334ee893d783ed41a
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-05 17:51:48 -0500

    Adding file 7 and updating file .h

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * leet - Entry point
+ * @l: Entry pointer
+ * Return: A variable 'l' modified.
+ */
+char *leet(char *l)
+{
+	int i = 0, j;
+	char l1[] = "aeotl"; 
+	char l2[] = "AEOTL";
+	char l3[] = "43071";
+
+	while (l[i] != '\0')
+	{
+		for (j = 0; l1[j] != '\0'; j++)
+		{
+			if (l[i] == l1[j] || l[i] == l2[j])
+			{
+				l[i] = l3[j];	
+			}
+		}
+		i++;
+	}
+	return (l);
+}
