22c52b8cd22c79c0e755c24d3a09236c0cec92c8
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-09 00:00:49 -0500

    Adding file 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[j] && haystack[i + 1 ] == needle[ i+1])
+		{
+			if(haystack[i] != ',' || (haystack[i] != ' ')
+				return (haystack + i);
+		}
+		j++;
+		i++;
+	}
+	return ('\0');
+}
