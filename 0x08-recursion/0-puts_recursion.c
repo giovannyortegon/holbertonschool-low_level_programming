51f59f6d13a28186f1e3eb636e1c273d6ea0fb08
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-10 13:58:16 -0500

    Adding file 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - entry point
+ * @s: Entry string
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		_putchar(_puts_recursion(s + 1));
+	}
+}
