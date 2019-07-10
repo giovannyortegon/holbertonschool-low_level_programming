ce84bb82d6fc8f6cd2255554af04e70adf158ecc
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-09 19:26:02 -0500

    Updating file 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stddef.h>
 
 /**
  * _strchr - entry point
@@ -16,5 +17,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		i++;
 	}
-	return ('\0');
+	return (NULL);
 }
