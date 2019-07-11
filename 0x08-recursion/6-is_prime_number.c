30136cfb7fcd93ffed0f642b82c2eedb35cc0563
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-10 23:33:30 -0500

    Addign file 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
 
+/*
+ * is_prime_number
+ * @n: entry variable
+ * Return: 0 if it isn't prime or 1 if it is.
+ */
 int is_prime_number(int n)
 {
 	int tmp = 0;
@@ -7,16 +12,14 @@ int is_prime_number(int n)
 	if (n == 1)
 	{
 		tmp = 0;
-		return 1;
+		return (1);
 	}
-	
-	if ( is_prime_number(n - 1) % 1 + is_prime_number(n - 1))
+	if (is_prime_number(n - 1) % 1 + is_prime_number(n - 1))
 	{
 		tmp++;
 	}
 	if (a != 2)
-		return 0;
+		return (0);
 	else
-		return 1;
-
+		return (1);
 }
