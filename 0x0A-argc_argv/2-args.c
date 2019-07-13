ec1c4e7ea7d3607ee3b56c57248f89c3f61ab3e9
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-13 15:52:57 -0500

    Adding file 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - entry pointt
+ * @argc: numbers of arguments
+ * @argv: argiments
+ * Return:  Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i <= argc - 1; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
