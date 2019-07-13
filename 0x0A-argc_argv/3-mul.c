ae816f398dcf45bce8603c550520ed24bab4cd3c
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-13 16:51:59 -0500

    Adding file 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - entry point
+ * @argc: count arguments
+ * @argv: vector of string
+ * Return:  Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int n1 = 0, n2 = 0, add = 0;
+	int n = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		n = 1;
+	}
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		add = n1 * n2;
+		printf("%d", add);
+	}
+	return (n);
+}
