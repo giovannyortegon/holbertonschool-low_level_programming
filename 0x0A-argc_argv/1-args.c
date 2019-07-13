84f39b93026d6b73e8216b4d53011de3498ab6af
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-13 15:38:42 -0500

    Adding file 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - entry point
+ * @argc:  count arguments
+ * @argv: arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int n = 0;
+
+	if (*(*argv + 1) == '\0')
+	{
+		printf("%d", n);
+	}
+	else
+	{
+		n = argc - 1;
+		printf("%d",  n);
+	}
+	return (0);
+}
