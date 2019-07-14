9be9f306b84de5823197fb170df931a3143fe4c1
Skillhh <ortegon.giovanny@hotmail.com>
2019-07-14 18:20:26 -0500

    Updating file 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,20 +9,19 @@
  */
 int main(int argc, char **argv)
 {
-	int n1 = 0, n2 = 0, mul = 0;
-	int n = 0;
+	double n1 = 0, n2 = 0, mul = 0;
 
 	if (argc < 2)
 	{
 		printf("Error\n");
-		n = 1;
+		return (1);
 	}
 	else
 	{
 		n1 = atoi(argv[1]);
 		n2 = atoi(argv[2]);
 		mul = n1 * n2;
-		printf("%d\n", mul);
+		printf("%.lf\n", mul);
+		return (0);
 	}
-	return (n);
 }
