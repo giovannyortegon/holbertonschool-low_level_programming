7afbf41a4ed24704c465edc03e30a923c8494f72
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-07-16 19:20:44 -0500

    Adding READEM.md

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - entry point
+ * @size: byte
+ * @c: Character to replace
+ * Return: Null if it is empty
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	s = (char *) malloc(size * sizeof(*s));
+	
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;	
+	}
+	return (s);
+}
