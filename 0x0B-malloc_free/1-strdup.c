a272c43e9b0580466c6515be745eac8daae362e4
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-07-16 20:14:20 -0500

    Updating file 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
- * strdup - call to pointer for copying in another
+ * _strdup - call to pointer for copying in another
  * @str: string pointer
  * Return: nstr is string in new pointer
  */
 char *_strdup(char *str)
 {
-	char *ns;
-	int i, size;
+	char *nstr;
+	int i, size = 0;
 
 	if (str == NULL)
 		return (NULL);
-	size = sizeof(str) + 1;
+	while (str[size] != '\0')
+		size++;
+	size += 1;
+
 	nstr = (char *) malloc(size * sizeof(char));
 	if (nstr == NULL)
 		return (NULL);
