38732c1b7862bb9408e832938650730f82264912
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-07-18 20:51:05 -0500

    Updating file 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,23 +9,19 @@
  */
 int *array_range(int min, int max)
 {
-	int i = 0;
+	int len_arr = 0;
 	int *ptr = NULL;
 
 	if (min > max)
-	{
 		return (NULL);
-	}
-	else
-	{
-		ptr = (int *) malloc((max + 1) * sizeof(int));
-		if (ptr == NULL)
-			return (ptr);
-		for (i = 0; i <= max; i++)
-		{
-			ptr[i] = i;
-			ptr[i + 1] = '\0';
-		}
+	len_arr = max - min;
+	ptr = (int *) malloc((len_arr + 1) * sizeof(int));
+	if (ptr == NULL)
 		return (ptr);
+	for (min = 0; min <= max; min++)
+	{
+		ptr[min] = min;
+		ptr[min + 1] = '\0';
 	}
+	return (ptr);
 }
