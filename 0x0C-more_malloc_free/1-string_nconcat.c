2257f46666d08d9d873c7356ad8aa91511db149d
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-07-18 19:55:07 -0500

    Updating file 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, c1 = 0, c2 = 0;
+	unsigned int i = 0, j = 0, cnt = 0;
 	char *new = NULL;
 
 	if (s1 == NULL)
@@ -19,27 +19,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	while (s1[i])
 		i++;
-	c1 = i + 1;
 	while (s2[j])
 		j++;
-	c2 = j++;
-	new = (char *) malloc(c1 * sizeof(char));
-	if (new == NULL)
-		return (new);
-	for (i = 0; i < c1; i++)
-		new[i] = s1[i];
-	if (n >= c2)
+	if (n >= j)
 	{
-		new = (char *) realloc(new, ((c1 + c2) + 1) * sizeof(char));
-		for (j = 0; s2[j]; c1++, j++)
-			new[c1 - 1] = s2[j];
+		cnt = i + j;
+		new = (char *) malloc((cnt + 1) * sizeof(char));
+		for (i = 0; s1[i]; i++)
+			new[i] = s1[i];
+		for (j = 0 ; s2[j]; j++, i++)
+			new[i] = s2[j];
 	}
 	else
 	{
-		new = (char *) realloc(new, ((c1 + n) + 1) * sizeof(char));
-		for (j = 0; j < n; j++, c1++)
-			new[c1 - 1] = s2[j];
+		cnt = i + n;
+		new = (char *) malloc((cnt + 1) * sizeof(char));
+		for (i = 0; s1[i]; i++)
+			new[i] = s1[i];
+		for (j = 0 ; j < n; j++, i++)
+			new[i] = s2[j];
 	}
-	new[c1 + 1] = '\0';
+	new[i + 1] = '\0';
 	return (new);
 }
