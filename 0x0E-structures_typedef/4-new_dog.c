9030ea8898860af091bfdc5791d7ec6c5fa58b94
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-07-22 22:54:30 -0500

    Reserve memory to structu and pointers

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,16 +11,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
-
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
+		return (NULL);	
+	char *nm = malloc(sizeof(*nm) + 1);
+	if (nm == NULL)
 		return (NULL);
+	char *on = malloc(sizeof(*nm) + 1);
+	if (on == NULL)
+		return (NULL);
+
 	if (name == NULL)
+	{
+		free(p);	
+		free(nm);
+		free(on);
 		return (NULL);
-	p->name = name;
+	}
+	else
+	{
+		nm = name;
+		p->name = nm;
+	}
 	p->age = age;
 	if (owner == NULL)
+	{
+		free(p);
+		free(nm);
+		free(on);
 		return (NULL);
-	p->owner = owner;
+	}
+	else
+	{
+		on = owner;
+		p->owner = owner;
+	}
 	return (p);
 }
