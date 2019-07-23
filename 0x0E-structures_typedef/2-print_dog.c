fe70e4bc7d1e52197096290c96a8cd988e47ee04
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-07-22 22:03:26 -0500

    Updating if pointer is NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,13 +7,16 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
-		printf("Name: %p\n", (void *)d->name);
-	else
-		printf("Name: %s\n",  d->name);
-	printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner: %p\n", (void *)d->owner);
-	else
-		printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: %p\n", (void *)d->name);
+		else
+			printf("Name: %s\n",  d->name);
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: %p\n", (void *)d->owner);
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
