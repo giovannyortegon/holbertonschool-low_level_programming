91d27cf5f7311da6305a01d6ac8e47ffd289976f
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-07-24 17:18:10 -0500

    Updating file 2 change && by ||

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,13 +9,11 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int sz = 0;
+	int sz;
 
-	if (array == NULL && cmp == NULL)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
-	if (size <= 0)
-		return (-1);
-	for (; sz < size; sz++)
+	for (sz = 0; sz < size; sz++)
 	{
 		if (cmp(array[sz]) != 0)
 			return (sz);
