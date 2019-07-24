42e6eb867caeef3e4d49fda166c414ac3df1ac47
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-07-24 13:49:03 -0500

    Adding file 1-array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - entry point
+ * @array:  pointer of an array from main
+ * @size:	lenght of array
+ * @action:  is a void pointer function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int x))
+{
+	size_t i;
+
+	for (i = 0U; i < size; ++i)
+		action(array[i]);
+}
