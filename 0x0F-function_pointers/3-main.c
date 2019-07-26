cabea6efa62e308c7f456055d633b5cb2119c44a
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-07-26 00:25:25 -0500

    [Update] Introduce a switch

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,22 +21,24 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%' )&& num2 == 0)
-	{
-		printf("Error\n");	
-		exit(100);
-	}
-
-
-	if (argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%')
-	{
-		calc = (*get_op_func(argv[2]))(num1, num2);
-		printf("%d\n", calc);
-	}
-	else
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
+	}
+	switch (argv[2][0])
+	{
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%':
+			calc = (*get_op_func(argv[2]))(num1, num2);
+			printf("%d\n", calc);
+		break;
+		default:
+			printf("Error\n");
+			exit(99);
 	}
 	/* Print operation */
 	return (0);
