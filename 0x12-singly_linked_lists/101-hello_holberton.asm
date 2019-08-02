a5b5c5f9eb8c5d08bb8a74991e48039d298621bf
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-08-01 20:24:30 -0500

    [UPDATE]

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,21 @@
+	extern	printf
 
-section .data
-msg:	db "Hello, Holberton", 0 	; C string need 0 
-fmt:	db "%s", 10, 0			; The prinf format, "\n", 0
+	section .data
+msg:	db "Hello, Holberton", 0	; C string needs 0
+fmt:	db "%s", 10, 0          	; The printf format, "\n",'0' 
 
-section .text			; Code section.
+	section .text			; Code section.
 
-	global	main		; Standar gcc
-	extern printf
-
+	global main
 main:
+	push    rbp
 	
-	push	rbp
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+	call    printf	; Call C function
 
-	mov		rdi, fmt
-	mov		rsi, msg
-	mov		rax, 0		
-	call	printf		; Call C function
+	pop	rbp		; restore stack
 
-	push	rbp
-
-	mov		rax, 0		; normal, no error, return 0 value
-	ret					; return
+	mov	rax,0		; normal, no error, return value
+	ret
