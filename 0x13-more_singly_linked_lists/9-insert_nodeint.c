4b47e7a91f597e6d5854ad6688669ada9e050666
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-08-07 20:53:10 -0500

    [UPDATE]

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,11 +7,9 @@
  * @n: new date
  * Return: new node or NULL if it fails.
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_n, *curr;
-
 	unsigned int i = 0;
 
 	curr = (*head);
@@ -19,9 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_n = (listint_t *) malloc(sizeof(listint_t));
 	if (new_n == NULL)
 		return (NULL);
-
 	new_n->n = n;
-
 	if ((*head) == NULL)
 	{
 		(*head) = new_n;
@@ -34,24 +30,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = curr;
 		return ((*head));
 	}
-	else
+	while (curr != NULL)
 	{
-		while ( curr != NULL)
+		if (i == idx - 1)
 		{
-			if (i == idx - 1)
-			{			
-				new_n->next = curr->next;
-				curr->next = new_n;
-				return (curr);
-			}
-			else if (curr->next == NULL)
-			{
-				curr->next = new_n;
-				return (curr);
-			}
-			curr = curr->next;
-			i++;
+			new_n->next = curr->next;
+			curr->next = new_n;
+			return (curr);
 		}
+		else if (curr->next == NULL)
+		{
+			curr->next = new_n;
+			return (curr);
+		}
+		curr = curr->next;
+		i++;
 	}
 	return (curr);
 }
