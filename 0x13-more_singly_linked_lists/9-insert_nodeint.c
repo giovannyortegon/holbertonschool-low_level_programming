ea00ca32785eedfdda82bf0613c3dd4c82d8a33c
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-08-07 20:28:15 -0500

    [FIX]

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = new_n;
 		return ((*head));
 	}
-	if(idx == 0)
+	if (idx == 0)
 	{
 		new_n->next = curr;
 		curr = new_n;
@@ -41,7 +41,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_n->next = curr->next;
 	curr->next = new_n;
-	
-	
 	return (curr);
 }
