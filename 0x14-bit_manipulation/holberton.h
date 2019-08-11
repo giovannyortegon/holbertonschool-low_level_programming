31f365c023d2d2ca2942382295dd0f10bf77584a
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-08-11 11:19:36 -0500

    [UPDATE] Holberton.h

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -9,5 +9,6 @@ int _pow(int base, int exp);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int mask_bin(unsigned long int n);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
