4d288bbde00311df49f1113f1a66c947e9a28d78
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-08-13 17:00:32 -0500

    [ADD] file

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+int _strlen(char *buf);
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* HOLBERTON_H */
