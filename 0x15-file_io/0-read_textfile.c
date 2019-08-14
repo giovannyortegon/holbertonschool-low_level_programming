2244da237cd0157972c3c15f663898a613a370e2
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-08-13 20:26:34 -0500

    [UPDATE]

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,11 +10,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char buf[1024];
 	int fd;
 
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1 || filename == NULL)
+	if (filename == NULL)
 		return (0);
 
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	
 	read(fd, buf, letters);
 	buf[letters] = '\0';
 
