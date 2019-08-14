859c14adc0f62719ccea1024a6837a0a5e43cabf
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-08-13 23:22:06 -0500

    [UPDATE]

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -9,11 +9,16 @@
 #include <fcntl.h>
 
 #include <unistd.h>
+#include <errno.h>
 
 int _strlen(char *buf);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+int copy_file(const char *filename, const char *file_new);
+void free_buf(char *buf);
+void close_f(int fd);
 
 #endif /* HOLBERTON_H */
