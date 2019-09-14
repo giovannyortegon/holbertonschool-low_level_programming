2dde96851047f01b4a6dd534322148da892c493f
Skillhh <ortegon.giovanny@hotmail.com>
2019-09-13 19:39:57 -0700

    [ADD] file 3

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - entry list
+ * @head: list main
+ * @n: value for new node
+ * Return: list modified
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *curr;
+
+	curr = (*head);
+
+	new = (dlistint_t *) malloc(sizeof(dlistint_t));
+	/* create node */
+	new->n = n;
+	new->next = NULL;
+	/* if it is empty */
+	if ((*head) == NULL)
+	{
+		new->prev = NULL;
+		(*head) = new;
+		return (*head);
+	}
+	/* search NULL it is last podsition of the node */
+	while (curr->next != NULL)
+		curr = curr->next;
+	/* and connect last node with the prev of new node */
+	new->prev = curr;
+	/* connect new node with next of last node */
+	curr->next = new;
+	return (curr);
+}
