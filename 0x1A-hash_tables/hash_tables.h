50a2c569110f28c97a4127984238f063b86dd36e
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-10-11 16:21:27 -0500

    [UPDATE] library

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -30,7 +30,8 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
-
+/* Fucntion hash tables */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
