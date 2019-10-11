861d1cd8b909eedf5862aa13955f1008bebb58ab
Giovanny Ortegon <ortegon.giovanny@hotmail.com>
2019-10-11 15:30:23 -0500

    [FIX] bettystyle

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_h = NULL;
+
+	new_h = malloc(sizeof(hash_table_t));
+	if (new_h == NULL)
+		return (NULL);
+
+	new_h->size = size;
+	new_h->array = malloc(sizeof(hash_table_t *) * size);
+	if (new_h->array == NULL)
+	{
+		free(new_h);
+		return (NULL);
+	}
+	return (new_h);
+}
