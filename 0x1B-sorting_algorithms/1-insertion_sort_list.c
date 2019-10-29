462484235827b1a14090d9b9f862e8d3d63f0e3a
Skillhh <ortegon.giovanny@hotmail.com>
2019-10-29 14:28:06 -0500

    [UPDATE]

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -7,7 +7,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr = (*list)->next;
 
-	if (list == NULL || *list == NULL || (*list) == NULL)
+	if (list == NULL || (*list)->next == NULL)
 		return;
 
 	while (curr != NULL)
