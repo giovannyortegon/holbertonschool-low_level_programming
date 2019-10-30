90d3cb8304c86ffae8dbf2d891e5f2bf0d3f6ad7
Skillhh <ortegon.giovanny@hotmail.com>
2019-10-29 20:56:10 -0500

    [ADD] file 3 quick sort

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,7 +1,4 @@
 #include "sort.h"
-#include <stdio.h>
-
-
 /**
  * quick_sort - sort array
  * @array:	array of number
@@ -9,23 +6,24 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	quickSort(array, 0, size - 1);
+	quickSort(array, 0, size - 1, size);
 }
 /**
  * quickSort - Sort Array
  * @array:	array of number
  * @low:	start index
  * @high:	ending index
+ * @size:	original legth of array
  */
-void quickSort(int *array, int low, int high)
+void quickSort(int *array, int low, int high, int size)
 {
 	int p_idx;
 
 	if (low < high)
 	{
-		p_idx = partition(array, low, high);
-		quickSort(array, low, p_idx - 1);
-		quickSort(array, p_idx + 1, high);
+		p_idx = partition(array, low, high, size);
+		quickSort(array, low, p_idx - 1, size);
+		quickSort(array, p_idx + 1, high, size);
 	}
 }
 /**
@@ -33,9 +31,10 @@ void quickSort(int *array, int low, int high)
  * @array:	array of number
  * @low:	start index
  * @high:	ending index
+ * @size:	original legth of array
  * Return:	value index
  */
-int partition(int *array, int low, int high)
+int partition(int *array, int low, int high, int size)
 {
 	int pivot  = array[high];
 	int i, j;
@@ -48,13 +47,13 @@ int partition(int *array, int low, int high)
 		{
 			i++;
 			swap(&array[i], &array[j]);
-			if (high + 1 == 10)
-			{
-				print_array(array, high + 1);
-			}
+			if (i != j)
+				print_array(array, size);
 		}
 	}
 	swap(&array[i + 1], &array[high]);
+	if (i + 1 != high)
+		print_array(array, size);
 	return (i + 1);
 }
 /**
