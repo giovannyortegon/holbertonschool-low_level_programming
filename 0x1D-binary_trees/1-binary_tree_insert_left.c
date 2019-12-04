d77c37a97ea7675add05feebc0a613091e2a3ff6
Skillhh <ortegon.giovanny@hotmail.com>
2019-12-04 18:42:45 -0500

    [UPDATE] free curr

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -21,6 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = binary_tree_node(parent, value);
 		parent = parent->left;
 		parent->left = binary_tree_node(parent, curr->n);
+		free(curr);
 	}
 	return (parent);
 }
