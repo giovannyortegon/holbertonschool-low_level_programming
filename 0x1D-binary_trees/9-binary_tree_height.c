430c02fe4c9dfc2d6f0dbbe6663027819874e605
Skillhh <ortegon.giovanny@hotmail.com>
2019-12-04 22:55:29 -0500

    [FIX] Fixing count and [UPDATE] library

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -16,7 +16,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		count = binary_tree_height(tree->left);
 		count = binary_tree_height(tree->right);
-		if (tree->right != NULL || tree->left)
+		if (tree->right != NULL || tree->left != NULL)
 			count++;
 	}
 	return (count);
