96c58736934dfdec7d910fc21479673e914f84ce
Skillhh <ortegon.giovanny@hotmail.com>
2019-12-04 23:03:59 -0500

    [FIX] Fixing count

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t count = 0;
 
-	if (tree == NULL || (!tree->left && !tree->right))
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	if (tree != NULL)
