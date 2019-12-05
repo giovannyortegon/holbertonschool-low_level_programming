7f9835442a3aad15b1fe28274335fb8189ac989f
Skillhh <ortegon.giovanny@hotmail.com>
2019-12-04 19:44:44 -0500

    [ADD] file 4

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -7,10 +7,8 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || (node->left != NULL && node->right != NULL))
 		return (0);
-	if (node->parent != NULL && (node->left == NULL && node->right == NULL))
-		return (1);
 
-	return (0);
+	return (1);
 }
