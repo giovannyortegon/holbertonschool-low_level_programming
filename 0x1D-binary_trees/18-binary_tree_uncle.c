b30fa925e26be8dc6d00e0689347d43d243360e9
Skillhh <ortegon.giovanny@hotmail.com>
2019-12-06 21:27:49 -0500

    [FIX] comparetion

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || !node->parent->parent)
 		return (NULL);
 
-	if (node->n < node->parent->n)
+	if (node->parent->n < node->parent->parent->n)
 		return (node->parent->parent->left);
 	else
 		return (node->parent->parent->right);
